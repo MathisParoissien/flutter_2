f6e386a480aed38c863ffc0b3f78150b2f331931
vincentvBTB <66956503+vincentvBTB@users.noreply.github.com>
2021-11-08 14:53:31 +0100

    rework for the front and merging it with the api call

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,29 +1,22 @@
+import 'package:Cook/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:Cook/navigation.dart';
 
-
-void main(){
-
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
-      .then((_){
-    runApp(MyApp());
-  }
-  );
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: Colors.white60),
-      home: const NavPage(title: 'Cook my life',),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       debugShowCheckedModeBanner: false,
+      home: HomeScreen()
     );
   }
 }
